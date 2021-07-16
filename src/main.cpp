e62f4271a8db73228bbd6f3e5422cf0d820efb81
Aiden <marcin.madry@interia.com>
2021-07-17 01:21:55 +0200

    Added keyboard support

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,83 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 SDL_Texture* texture = nullptr;
 
+bool initializeSDL(const std::string& title, int width, int height);
+void parseMovementKeyDown(SDL_Keycode key, Chip8& cripsy);
+void parseMovementKeyUp(SDL_Keycode key, Chip8& cripsy);
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+    std::cerr << "Please provide ROM you want to run\n";
+    return 1;
+  }
+
+  Chip8 crispy;
+  crispy.initialize();
+
+  if (!crispy.loadROM(argv[1])) {
+    return 1;
+  }
+
+  const int SCALE = 16;
+
+  int width = 64 * SCALE;
+  int height = 32 * SCALE;
+
+  SDL_Event event;
+  uint32_t pixelBuffer[2048];
+
+  bool isOpen = initializeSDL("CrispyChip - CHIP8 Emulator", width, height);
+
+  while (isOpen) {
+    crispy.emulateCycle();
+
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          isOpen = false;
+          break;
+        case SDL_KEYDOWN: {
+          if (event.key.keysym.sym == SDLK_ESCAPE) {
+            isOpen = false;
+          } else {
+            parseMovementKeyDown(event.key.keysym.sym, crispy);
+          }
+        } break;
+        case SDL_KEYUP:
+          parseMovementKeyUp(event.key.keysym.sym, crispy);
+          break;
+      }
+    }
+
+    if (crispy.render) {
+      for (int i = 0; i < 2048; ++i) {
+        if (crispy.display[i]) {
+          pixelBuffer[i] = 0xFFFFFFFF;
+        } else {
+          pixelBuffer[i] = 0x000000FF;
+        }
+      }
+
+      SDL_UpdateTexture(texture, nullptr, pixelBuffer, 64 * sizeof(uint32_t));
+      SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+
+      crispy.render = false;
+    }
+    
+    SDL_RenderPresent(renderer);
+    SDL_Delay(1);
+  }
+
+  // Clean up
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
+
 bool initializeSDL(const std::string& title, int width, int height)
 {
   if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -56,65 +133,122 @@ bool initializeSDL(const std::string& title, int width, int height)
   return true;
 }
 
-int main(int argc, char* argv[])
+void parseMovementKeyDown(SDL_Keycode key, Chip8& crispy)
 {
-  if (argc < 2) {
-    std::cerr << "Please provide ROM you want to run\n";
-    return 1;
+  switch (key) {
+    case SDLK_1:
+      crispy.keyboard[0x1] = 1;
+      break;
+    case SDLK_2:
+      crispy.keyboard[0x2] = 1;
+      break;
+    case SDLK_3:
+      crispy.keyboard[0x3] = 1;
+      break;
+    case SDLK_4:
+      crispy.keyboard[0xC] = 1;
+      break;
+
+
+    case SDLK_q:
+      crispy.keyboard[0x4] = 1;
+      break;
+    case SDLK_w:
+      crispy.keyboard[0x5] = 1;
+      break;
+    case SDLK_e:
+      crispy.keyboard[0x6] = 1;
+      break;
+    case SDLK_r:
+      crispy.keyboard[0xD] = 1;
+      break;
+
+
+    case SDLK_a:
+      crispy.keyboard[0x7] = 1;
+      break;
+    case SDLK_s:
+      crispy.keyboard[0x8] = 1;
+      break;
+    case SDLK_d:
+      crispy.keyboard[0x9] = 1;
+      break;
+    case SDLK_f:
+      crispy.keyboard[0xE] = 1;
+      break;
+
+
+    case SDLK_z:
+      crispy.keyboard[0xA] = 1;
+      break;
+    case SDLK_x:
+      crispy.keyboard[0x0] = 1;
+      break;
+    case SDLK_c:
+      crispy.keyboard[0xB] = 1;
+      break;
+    case SDLK_v:
+      crispy.keyboard[0xF] = 1;
+      break;
   }
+}
 
-  Chip8 crispy;
-  crispy.initialize();
+void parseMovementKeyUp(SDL_Keycode key, Chip8& crispy)
+{
+  switch (key) {
+    case SDLK_1:
+      crispy.keyboard[0x1] = 0;
+      break;
+    case SDLK_2:
+      crispy.keyboard[0x2] = 0;
+      break;
+    case SDLK_3:
+      crispy.keyboard[0x3] = 0;
+      break;
+    case SDLK_4:
+      crispy.keyboard[0xC] = 0;
+      break;
 
-  if (!crispy.loadROM(argv[1])) {
-    return 1;
+
+    case SDLK_q:
+      crispy.keyboard[0x4] = 0;
+      break;
+    case SDLK_w:
+      crispy.keyboard[0x5] = 0;
+      break;
+    case SDLK_e:
+      crispy.keyboard[0x6] = 0;
+      break;
+    case SDLK_r:
+      crispy.keyboard[0xD] = 0;
+      break;
+
+
+    case SDLK_a:
+      crispy.keyboard[0x7] = 0;
+      break;
+    case SDLK_s:
+      crispy.keyboard[0x8] = 0;
+      break;
+    case SDLK_d:
+      crispy.keyboard[0x9] = 0;
+      break;
+    case SDLK_f:
+      crispy.keyboard[0xE] = 0;
+      break;
+
+
+    case SDLK_z:
+      crispy.keyboard[0xA] = 0;
+      break;
+    case SDLK_x:
+      crispy.keyboard[0x0] = 0;
+      break;
+    case SDLK_c:
+      crispy.keyboard[0xB] = 0;
+      break;
+    case SDLK_v:
+      crispy.keyboard[0xF] = 0;
+      break;
   }
-
-  const int SCALE = 16;
-
-  int width = 64 * SCALE;
-  int height = 32 * SCALE;
-
-  SDL_Event event;
-  uint32_t pixelBuffer[2048];
-
-  bool isOpen = initializeSDL("CrispyChip - CHIP8 Emulator", width, height);
-
-  while (isOpen) {
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          isOpen = false;
-          break;
-      }
-    }
-
-    crispy.emulateCycle();
-
-    if (crispy.render) {
-      for (int i = 0; i < 2048; ++i) {
-        if (crispy.display[i]) {
-          pixelBuffer[i] = 0xFFFFFFFF;
-        } else {
-          pixelBuffer[i] = 0x000000FF;
-        }
-      }
-
-      SDL_UpdateTexture(texture, nullptr, pixelBuffer, 64 * sizeof(uint32_t));
-      SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-
-      crispy.render = false;
-    }
-    
-    SDL_RenderPresent(renderer);
-    SDL_Delay(1);
-  }
-
-  // Clean up
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
 }
