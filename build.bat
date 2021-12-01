b8d913f68902834e439734692d1d5bcad31d0eea
Aiden <marcin.madry@interia.com>
2021-12-02 00:00:13 +0100

    Change building the project

--- build.bat
+++ build.bat
@@ -3,12 +3,12 @@
 set CC=g++
 
 set FILES=src/main.cpp src/chip8.cpp src/smol_renderer.cpp
-set FLAGS=-Os -w -s -std=c++17
+set FLAGS=-Os -w -s -std=c++17 -static-libgcc -static-libstdc++
 
 set INCLUDE=-I"include"
 set LIBS=-L"libs"
 
-set LINKER=-l"SDL2/SDL2" -l"SDL2/SDL2main" -l"SDL2_ttf/SDL2_ttf"
+set LINKER=-l"SDL2/SDL2" -l"SDL2/SDL2main" -l"SDL2_ttf/SDL2_ttf" -lwinpthread
 
 if not exist build_dir mkdir build_dir
 if not exist "build_dir\SDL2.dll" copy "libs\SDL2\SDL2.dll" "build_dir\SDL2.dll"
