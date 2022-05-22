092fb7b0e448b590d07d5f981515c6f9c12759da
Aiden <marcin.madry@interia.com>
2022-05-22 22:32:37 +0200

    Change building, add MSVC build batch file

--- build.bat
+++ build.bat
@@ -10,10 +10,4 @@ set LIBS=-L"libs"
 
 set LINKER=-l"SDL2/SDL2" -l"SDL2/SDL2main" -l"SDL2_ttf/SDL2_ttf" -lwinpthread
 
-if not exist build_dir mkdir build_dir
-if not exist "build_dir\SDL2.dll" copy "libs\SDL2\SDL2.dll" "build_dir\SDL2.dll"
-if not exist "build_dir\SDL2_ttf.dll" copy "libs\SDL2_ttf\SDL2_ttf.dll" "build_dir\SDL2_ttf.dll"
-if not exist "build_dir\libfreetype-6.dll" copy "libs\SDL2_ttf\libfreetype-6.dll" "build_dir\libfreetype-6.dll"
-if not exist "build_dir\zlib1.dll" copy "libs\SDL2_ttf\zlib1.dll" "build_dir\zlib1.dll"
-
 call %CC% %FILES% %INCLUDE% %LIBS% %FLAGS% %LINKER% -o build_dir/CrispyChip
