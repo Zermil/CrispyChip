f47a0b8fcb06aa300bb910355d04d7907a0344e5
Aiden <marcin.madry@interia.com>
2023-05-19 14:45:08 +0200

    Make the code compile under new changes

---
+++ code/chip8.cpp
@@ -0,0 +1,365 @@
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "chip8.hpp"
+
+void Chip8::initialize() 
+{
+    memset(keyboard, 0, 16);
+    memset(display, 0, 2048);
+    memset(stack, 0, 16);
+    memset(memory, 0, 4096);
+    memset(V, 0, 16);
+
+    PC = 0x200;
+    SP = 0;
+    I = 0;
+
+    for (int i = 0; i < 80; ++i) {
+        memory[i] = font_sprites[i];
+    }
+
+    delay_timer = 0;
+    sound_timer = 0;
+
+    render = true;
+
+    // Some instructions/opcodes need random byte
+    srand((unsigned int) time(0));
+}
+
+void Chip8::emulateCycle()
+{
+    // Get opcode
+    uint16_t opcode = memory[PC] << 8 | memory[PC + 1];
+
+    uint16_t addr   = opcode & 0x0FFF;      // address, lowest 12 bits
+
+    uint8_t  n      = opcode & 0x000F;      // lowest 4 bits
+    uint8_t  kk     = opcode & 0x00FF;      // lowest 8 bits
+    uint8_t  x      = opcode >> 8 & 0x000F; // 4 bit value, lower 4 bits of the high byte
+    uint8_t  y      = opcode >> 4 & 0x000F; // 4 bit value, upper 4 bits of the high byte
+  
+    // OP-codes, implementations
+    switch (opcode & 0xF000) {
+        case 0x0000: {
+            switch (n) {
+                case 0x0000: // Clear screen
+                    memset(display, 0, 2048);
+                    render = true;
+                    PC += 2;
+                    break;
+
+                case 0x000E: // Return from subroutine
+                    --SP;
+                    PC = stack[SP];
+                    PC += 2;
+                    break;
+
+                default:
+                    std::cerr << "Unknown opcode: " << opcode << std::endl;
+            }
+        } break;
+    
+        case 0x1000: // Jump to location
+            PC = addr;
+            break;
+
+        case 0x2000: // Call subroutine
+            stack[SP] = PC;
+            ++SP;
+            PC = addr;
+            break;
+
+        case 0x3000: // Skip next instruction if V[x] == kk
+            if (V[x] == kk) {
+                PC += 2;
+            }
+            PC += 2;
+            break;
+
+        case 0x4000: // Skip next instruction if V[x] != kk
+            if (V[x] != kk) {
+                PC += 2;
+            } 
+            PC += 2;
+            break;
+
+        case 0x5000: // Skip next instruction if V[x] == V[y]
+            if (V[x] == V[y]) {
+                PC += 2;
+            }
+            PC += 2;
+            break;
+
+        case 0x6000: // Set V[x] = kk
+            V[x] = kk;
+            PC += 2;
+            break;
+
+        case 0x7000: // Set V[x] += kk
+            V[x] += kk;
+            PC += 2;
+            break;
+    
+        case 0x8000: {
+            switch (n) {
+                case 0x0000: // Set V[x] = V[y]
+                    V[x] = V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0001: // Set V[x] = V[x] | V[y]
+                    V[x] |= V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0002: // Set V[x] = V[x] & V[y]
+                    V[x] &= V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0003: // Set V[x] = V[x] ^ V[y]
+                    V[x] ^= V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0004: // Set V[x] += V[y], set V[0xF] = carry
+                    V[0xF] = (V[x] + V[y] > 0xFF) ? 1 : 0;
+                    V[x] += V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0005: // Set V[x] -= V[y], set V[0xF] = NOT borrow
+                    V[0xF] = (V[x] > V[y]) ? 1 : 0; 
+                    V[x] -= V[y];
+                    PC += 2;
+                    break;
+
+                case 0x0006: // Set V[x] = V[x] SHR 1
+                    V[0xF] = V[x] & 0x1; 
+                    V[x] >>= 1;
+                    PC += 2;
+                    break;
+
+                case 0x0007: // Set V[x] = V[y] - V[x], set V[0xF] = NOT borrow
+                    V[0xF] = (V[y] > V[x]) ? 1 : 0; 
+                    V[x] = V[y] - V[x];
+                    PC += 2;
+                    break;
+
+                case 0x000E: // Set V[x] = V[x] SHL 1
+                    V[0xF] = V[x] >> 7;
+                    V[x] <<= 1;
+                    PC += 2;
+                    break;
+
+                default:
+                    std::cerr << "Unknown opcode: " << opcode << std::endl;
+            }
+        } break;
+
+        case 0x9000: // Skip next instruction if V[x] != V[y]
+            if (V[x] != V[y]) {
+                PC += 2;
+            }
+            PC += 2;
+            break;
+
+        case 0xA000: // Set I = addr
+            I = addr;
+            PC += 2;
+            break;
+
+        case 0xB000: // Jump to addr + V[0]
+            PC = V[0x0] + addr;
+            break;
+
+        case 0xC000: // Set V[x] to random byte & kk
+            V[x] = (rand() % 0xFF) & kk;
+            PC += 2;
+            break;
+
+            // This is probably the weirdest instruction in CHIP8
+        case 0xD000: { // Display n-byte sprite starting at memory location I at (V[x], V[y]), set V[0xF] = collision
+            uint8_t Vx = V[x];
+            uint8_t Vy = V[y];
+            uint8_t pixel; 
+            V[0xF] = 0;
+
+            for (int y0 = 0; y0 < n; ++y0) {
+                pixel = memory[I + y0];
+
+                for (int x0 = 0; x0 < 8; ++x0) {
+                    if ((pixel & (0x80 >> x0)) != 0) {
+                        // index = x + width*y (% loop around)
+                        int index = ((Vx + x0) + ((Vy + y0) * 64)) % 2048;
+
+                        if (display[index] == 1) {
+                            V[0xF] = 1;
+                        }
+
+                        display[index] ^= 1;
+                    }
+                }
+            }
+      
+            render = true;
+            PC += 2;
+        } break;
+
+        case 0xE000: {
+            switch (n) {
+                case 0x000E: // Skip instruction if key at V[x] is pressed
+                    if (keyboard[V[x]] != 0) {
+                        PC += 2;
+                    }
+                    PC += 2;
+                    break;
+
+                case 0x0001: // Skip instruction if key at V[x] is NOT pressed
+                    if (keyboard[V[x]] == 0) {
+                        PC += 2;
+                    }
+                    PC += 2;
+                    break;
+
+                default:
+                    std::cerr << "Unknown opcode: " << opcode << std::endl;
+            }
+        } break;
+
+        case 0xF000: {
+            switch (kk) {
+                case 0x0007: // Set Vx = delay timer value
+                    V[x] = delay_timer;
+                    PC += 2;
+                    break;
+
+                case 0x000A: { // Wait for a key press, store the value of the key in Vx
+                    bool keyPress = false;
+
+                    for (uint8_t i = 0; i < 16; ++i) {
+                        if (keyboard[i] != 0) {
+                            V[x] = i;
+                            keyPress = true;
+                        }
+                    }
+
+                    if (!keyPress) {
+                        return;
+                    }
+
+                    PC += 2;
+                } break;
+
+                case 0x0015: // Set delay timer = Vx
+                    delay_timer = V[x];
+                    PC += 2;
+                    break;
+
+                case 0x0018: // Set sound timer = Vx
+                    sound_timer = V[x];
+                    PC += 2;
+                    break;
+
+                case 0x001E: // Set I = I + Vx
+                    V[0xF] = (I + V[x] > 0xFFF) ? 1 : 0;
+                    I += V[x];
+                    PC += 2;
+                    break;
+
+                case 0x0029: // Set I = location of sprite for digit Vx
+                    // We multiply by 5 because we want the "starting index" of a sprite
+                    I = V[x] * 0x5;
+                    PC += 2;
+                    break;
+
+                case 0x0033: // Store BCD representation of Vx in memory locations I, I+1, and I+2
+                    memory[I]     = V[x] / 100;
+                    memory[I + 1] = (V[x] / 10) % 10;
+                    memory[I + 2] = V[x] % 10;
+                    PC += 2;
+                    break;
+
+                case 0x0055: // Store registers V0 through Vx in memory starting at location I 
+                    for (int i = 0; i <= x; ++i) {
+                        memory[I + i] = V[i]; 
+                    }
+                    I += x + 1; // Staying true to the original
+                    PC += 2;
+                    break;
+
+                case 0x0065: // Read registers V0 through Vx from memory starting at location I 
+                    for (int i = 0; i <= x; ++i) {
+                        V[i] = memory[I + i]; 
+                    }
+                    I += x + 1; // Staying true to the original
+                    PC += 2;
+                    break;
+
+                default:
+                    std::cerr << "Unknown opcode: " << opcode << std::endl;
+            }
+        } break;
+
+        default:
+            std::cerr << "Unknown opcode: " << opcode << std::endl;
+    }
+
+    // Timers, unsigned so that's why the if "guards"
+    if (delay_timer > 0) {
+        delay_timer--;
+    }
+
+    if (sound_timer > 0) {
+        if (sound_timer == 1) {
+            std::cout << "SOUND\n";
+        }
+
+        sound_timer--;
+    }
+}
+
+bool Chip8::loadROM(const std::string& rom_path)
+{
+    std::ifstream rom_file(rom_path, std::ios::binary | std::ios::ate);
+
+    if (!rom_file.is_open()) {
+        std::cerr << "Invalid path to ROM provided\n";
+        return false;
+    }
+
+    std::streamsize size = rom_file.tellg();
+
+    // Check if file size is appropriate
+    if (size > 4096 - 512 || size <= 0) {
+        std::cerr << "Invalid file size, make sure the ROM fits CHIP8 memory\n";
+        rom_file.close();
+        return false;
+    }
+
+    uint8_t* buffer = new uint8_t[size];
+
+    if (buffer == nullptr) {
+        std::cerr << "Failed to allocate buffer\n";
+        rom_file.close();
+        return false;
+    }
+  
+    rom_file.seekg(0, std::ios::beg);
+    rom_file.read(reinterpret_cast<char*>(buffer), size);
+
+    // First 512 bytes are reserved
+    for (int i = 0; i < size; ++i) {
+        memory[i + 512] = buffer[i];
+    }
+
+    // Clean up
+    rom_file.close();
+    delete[] buffer;
+
+    return true; 
+}
---
+++ code/smol_renderer.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include "smol_renderer.hpp"
+
+Renderer::Renderer(const std::string& title)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Failed to initialize SDL\n";
+        error = true;
+    }
+
+    if (TTF_Init() != 0) {
+        std::cerr << "Failed to initialize SDL_ttf\n";
+        error = true;
+    }
+
+    window = SDL_CreateWindow(
+        title.c_str(),
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WIDTH,
+        HEIGHT,
+        SDL_WINDOW_SHOWN
+        );
+
+    if (window == nullptr) {
+        std::cerr << "Failed to initialize SDL Window\n";
+        error = true;
+    }
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+        );
+
+    if (renderer == nullptr) {
+        std::cerr << "Failed to initialize SDL Renderer\n";
+        error = true;
+    }
+
+    texture_crispy = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        64, 32
+        );
+
+    if (texture_crispy == nullptr) {
+        std::cerr << "Failed to initialize SDL Texture\n";
+        error = true;
+    }
+
+    font = TTF_OpenFont("Consolas.ttf", FONT_SIZE);
+}
+
+void Renderer::displayTextAt(const std::string& msg_text, SDL_Color& color, int y)
+{
+    SDL_Surface* font_surface = TTF_RenderText_Solid(font, msg_text.c_str(), color);
+
+    if (font_surface == nullptr) {
+        std::cerr << "Failed to create SDL_Surface\n";
+        return;
+    }
+
+    SDL_Texture* font_texture = SDL_CreateTextureFromSurface(renderer, font_surface);
+    SDL_FreeSurface(font_surface);
+
+    if (font_texture == nullptr) {
+        std::cerr << "Failed to create SDL_Texture\n";
+        return;
+    }
+
+    SDL_Rect font_rect = {};
+    SDL_QueryTexture(font_texture, nullptr, nullptr, &font_rect.w, &font_rect.h);
+
+    font_rect.x = (WIDTH / 2) - (font_rect.w / 2);
+    font_rect.y = y;
+
+    SDL_RenderCopy(renderer, font_texture, nullptr, &font_rect);
+    SDL_DestroyTexture(font_texture);
+}
+
+void Renderer::renderMenu(size_t index, const SDL_Rect& selector)
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &selector);
+
+    for (int i = 0; i < 4; ++i) {
+        SDL_Color color;
+
+        if (i == index) color = { 0, 0, 0, 255 };
+        else color = { 255, 255, 255, 255 };
+
+        displayTextAt(ROMS[i], color, 75 + (i * 120));
+    }
+
+    SDL_RenderPresent(renderer);
+}
+
+void Renderer::renderEmulation(Chip8& crispy)
+{
+    crispy.emulateCycle();
+
+    // Render is set to true after initialization, so it will render at least once, 
+    // in other words: it will clear the screen when first called.
+    if (crispy.render) {
+        uint32_t pixelBuffer[2048];
+
+        for (int i = 0; i < 2048; ++i) {
+            if (crispy.display[i]) pixelBuffer[i] = 0xFFFFFFFF;
+            else pixelBuffer[i] = 0x000000FF;
+        }
+
+        SDL_UpdateTexture(texture_crispy, nullptr, pixelBuffer, 64 * sizeof(uint32_t));
+        SDL_RenderCopy(renderer, texture_crispy, nullptr, nullptr);
+
+        crispy.render = false;
+    }
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(1);
+}
+
+Renderer::~Renderer()
+{
+    // Clean up
+    SDL_DestroyTexture(texture_crispy);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    TTF_CloseFont(font);
+    TTF_Quit();
+
+    SDL_Quit();
+}
